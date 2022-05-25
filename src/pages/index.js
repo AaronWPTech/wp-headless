6644a97c3edbfed2add3688bd9a406aa5a7a1774
AaronWPTech <aaron.mccann@wpengine.com>
2022-05-26 00:21:03 +0100

    Aaron made changes to the index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,8 +52,7 @@ export default function Page() {
               Welcome to your Blueprint
             </Heading>
             <p className={styles.description}>
-              Achieve unprecedented performance with modern frameworks and the
-              world&apos;s #1 open source CMS in one powerful headless platform.{' '}
+              Heh Aaron Has been making changes again.{' '}
             </p>
             <div className={styles.actions}>
               <Button styleType="secondary" href="/contact-us">
